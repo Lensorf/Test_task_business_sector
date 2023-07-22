f414391969fb76f6dd2ff1f88fb3ccea455d6708
Ilya <shiroku12@mail.ru>
2023-07-22 10:12:02 +0300

    deploy_v2

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ export default function App() {
 
   useMemo(() => {
     if (sortConfig !== null) {
-      sortedPosts?.sort((a, b) => {
+      currentPosts?.sort((a, b) => {
         if (a[sortConfig.key] < b[sortConfig.key]) {
           return sortConfig.direction === 'ascending' ? -1 : 1;
         }
@@ -56,7 +56,7 @@ export default function App() {
       });
     }
     return posts;
-  }, [sortedPosts, sortConfig]);
+  }, [currentPosts, sortConfig]);
   // -------------------------------------------------
 
   return (
